96b1a3586196bcbbfeae5d3fae5e54736728f85b
theravikr21 <ravi.kumar@preplaced.in>
2024-07-13 17:01:21 +0530

    lec 5 How to import packages in Golang | Golang full course

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"m/myutils"
+)
 
 func main() {
 	fmt.Println("Hello world")
+	myutils.MyProgram("Hii ravi")
 }
---
+++ myutils/myutils.go
@@ -0,0 +1,6 @@
+package myutils //same as folder name
+import "fmt"
+
+func MyProgram(message string) {  //func name start with capital letter
+	fmt.Println(message)
+}
